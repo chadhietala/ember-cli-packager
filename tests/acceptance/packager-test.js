b18f4dff44cb771ded4a08d0153fdc01a3a05926
Chad Hietala <chadhietala@gmail.com>
2015-05-11 14:10:31 -0700

    Building for development and production
    Instead of creating multiple files that magically get concated we will move them to main files provided by an addon.  This leaves suffix and prefix files that can safely be concated on existing bundles.

--- tests/acceptance/packager-test.js
+++ tests/acceptance/packager-test.js
@@ -4,6 +4,7 @@ var Packager = require('../../lib/packager');
 var expect = require('chai').expect;
 var broccoli = require('broccoli');
 var walkSync = require('walk-sync');
+var fs = require('fs');
 var cwd = process.cwd();
 
 describe('Packager', function() {
@@ -52,12 +53,15 @@ describe('Packager', function() {
         'dummy/components/baz-bar.js',
         'dummy/components/foo-bar.js',
         'dummy/config/environment.js',
+        'dummy/crossdomain.xml',
         'dummy/dep-graph.json',
         'dummy/index.html',
+        'dummy/index.js',
         'dummy/pods/bizz-buzz/component.js',
         'dummy/pods/bizz-buzz/template.js',
         'dummy/pods/foo-baz/component.js',
         'dummy/pods/foo-baz/template.js',
+        'dummy/robots.txt',
         'dummy/router.js',
         'dummy/styles/app.css',
         'dummy/templates/components/foo-bar.js',
@@ -70,26 +74,69 @@ describe('Packager', function() {
       ]);
     });    
   });
+  
+  describe('default concat strategy', function() {
 
-  it('should follow the default concat strategy', function() {
-    packager = new Packager({
-      entries: ['dummy']
+    it('should output the correct concat files', function() {
+      packager = new Packager({
+        entries: ['dummy']
+      });
+
+      var dist = packager.package();
+
+      builder = new broccoli.Builder(dist);
+
+      return builder.build().then(function(results) {
+
+        expect(listFiles(results.directory)).to.deep.eql([
+          'default-build/assets/dummy.css',
+          'default-build/assets/dummy.js',
+          'default-build/assets/dummy.map',
+          'default-build/assets/vendor.css',
+          'default-build/assets/vendor.js',
+          'default-build/assets/vendor.map',
+          'default-build/index.html'
+        ]);
+      });
     });
 
-    var dist = packager.package();
+    it('should include the tests and should be test mode', function() {
+      
+      packager = new Packager({
+        entries: ['dummy']
+      });
 
-    builder = new broccoli.Builder(dist);
+      var dist = packager.package();
 
-    return builder.build().then(function(results) {
-      expect(listFiles(results.directory)).to.deep.eql([
-        'default-build/assets/dummy.css',
-        'default-build/assets/dummy.js',
-        'default-build/assets/dummy.map',
-        'default-build/assets/vendor.css',
-        'default-build/assets/vendor.js',
-        'default-build/assets/vendor.map',
-        'default-build/index.html'
-      ]);
+      builder = new broccoli.Builder(dist);
+
+      return builder.build().then(function(results) {
+        var tests = fs.readFileSync(results.directory + '/default-build/assets/dummy.js', 'utf8');
+        expect(tests.indexOf('runningTests = true;')).to.be.gt(-1);
+        expect(tests.indexOf('moduleForComponent(\'foo-bar\'')).to.be.gt(-1);
+      });
     });
+
+
+    it('should not incude the tests and shouldn\'t be test mode when building for production', function() {
+      
+      process.env.EMBER_ENV = 'production';
+
+      packager = new Packager({
+        entries: ['dummy']
+      });
+
+      var dist = packager.package();
+
+      builder = new broccoli.Builder(dist);
+
+      return builder.build().then(function(results) {
+        var tests = fs.readFileSync(results.directory + '/default-build/assets/dummy.js', 'utf8');
+        expect(tests.indexOf('runningTests = true;')).to.be.lt(0);
+        expect(tests.indexOf('moduleForComponent(\'foo-bar\'')).to.be.lt(0);
+        process.env.EMBER_ENV = 'development';
+      });
+    });
+
   });
 });
